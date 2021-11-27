b4dcf47f89064b81ef3dcb615c069d626f9bcd8f
Andrew Starritt <andrew.starritt@gmail.com>
2021-11-27 14:11:25 +1100

    Remove depricated items from example plugin. Also use macros in the widget manager.

--- qeExamplePluginSup/project/widgets/exampleWidget/exampleWidgetManager.h
+++ qeExamplePluginSup/project/widgets/exampleWidget/exampleWidgetManager.h
@@ -1,5 +1,9 @@
-/*
- *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
+/*  exampleWidgetManager.h
+ *
+ *  This file is part of the EPICS QT Framework, initially developed at the
+ *  Australian Synchrotron.
+ *
+ *  Copyright (c) 2015-2021 Australian Synchrotron
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -14,44 +18,28 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright (c) 2015 Australian Synchrotron
- *
  *  Author:
  *    Andrew Rhyder
  *  Contact details:
  *    andrew.rhyder@synchrotron.org.au
  */
 
-#ifndef EXAMPLEWIDGETMANAGER_H
-#define EXAMPLEWIDGETMANAGER_H
+#ifndef EXAMPLE_WIDGET_MANAGER_H
+#define EXAMPLE_WIDGET_MANAGER_H
 
 #include <QEDesignerPluginCommon.h>
 #include <examplePluginLibrary_global.h>
 
-/*
-    ???
-*/
-class EXAMPLEPLUGINLIBRARYSHARED_EXPORT exampleWidgetManager : public QObject, public QDesignerCustomWidgetInterface {
-     Q_OBJECT
-     Q_INTERFACES(QDesignerCustomWidgetInterface)
+#include <QEDesignerPluginCommon.h>
+#include <QEFrameworkLibraryGlobal.h>
 
-  public:
-    exampleWidgetManager( QObject *parent = 0 );
+class QE_FRAMEWORK_LIBRARY_SHARED_EXPORT exampleWidgetManager:
+      public QObject, public QDesignerCustomWidgetInterface
+{
+   Q_OBJECT
+   Q_INTERFACES (QDesignerCustomWidgetInterface)
 
-    bool isContainer() const;
-    bool isInitialized() const;
-    QIcon icon() const;
-    //QString domXml() const;
-    QString group() const;
-    QString includeFile() const;
-    QString name() const;
-    QString toolTip() const;
-    QString whatsThis() const;
-    QWidget *createWidget( QWidget *parent );
-    void initialize( QDesignerFormEditorInterface *core );
-
-  private:
-    bool initialized;
+   QE_DECLARE_PLUGIN_MANAGER (exampleWidget)
 };
 
-#endif // EXAMPLEWIDGETMANAGER_H
+#endif  // EXAMPLE_WIDGET_MANAGER_H
